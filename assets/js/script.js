48949fd8d205152cbaff9e3150516f9d9c4b6f54
Hamszn <nicholaushamilton@gmail.com>
2024-06-17 22:41:44 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
-}g
+}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
